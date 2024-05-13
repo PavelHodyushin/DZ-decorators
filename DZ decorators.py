44e8cf9445ec2868a16c47f20e2e6e05eb744028
PavelHodyushin <161248557+PavelHodyushin@users.noreply.github.com>
2024-05-13 23:34:32 +0300

    Update DZ decorators.py

--- DZ decorators.py
+++ DZ decorators.py
@@ -1,14 +1,15 @@
-
 def is_prime(func):
     def wrapper(*args):
-        res = func(*args)
-        if res > 1:
-            print("Простое")
-        elif res == 1:
-            print("Число 1 не является ни простым, ни составным числом")
-        else:
-            print("Составное")
-        return res
+        n = func(*args)
+        if n <= 1:
+            print("Не простое")
+            return n
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                print("Составное")
+                return n
+        print("Простое")
+        return n
     return wrapper
 
 
@@ -18,7 +19,11 @@ def sum_three(*args):
 
 result = sum_three(2,3,6)
 print(result)
-result = sum_three(0,1,0)
+result = sum_three(3,3,3)
 print(result)
-result = sum_three(-1,0,0)
+result = sum_three(25,36,46)
+print(result)
+result = sum_three(43,78,21)
+print(result)
+result = sum_three(900,80,3)
 print(result)
